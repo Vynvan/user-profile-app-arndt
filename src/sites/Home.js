b4648decb6c0886999673859b304be84844953f5
vynvan_server <giant_innovations@live.de>
2024-11-01 09:51:08 +0100

    Added userContext to Home

--- src/sites/Home.js
+++ src/sites/Home.js
@@ -1,11 +1,16 @@
+import { UserContext } from '../components/UserProvider';
+import { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 
 function Home({ title }) {
-    return (
-        <Container className='component'>
-            <h1>Willkommen auf {title || 'dieser Profilseite' }</h1>
-        </Container>
-    );
+   const { user } = useContext(UserContext);
+
+   return (
+      <Container className='component'>
+         <h1>Willkommen auf {title || 'dieser Profilseite' }</h1>
+         {user && <p>Sie sind eingeloggt.</p>}
+      </Container>
+   );
 }
 
 export default Home
