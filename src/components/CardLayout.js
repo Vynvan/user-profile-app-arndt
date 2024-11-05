b3c896c3f679a911c1a397ee87616c1b370a4a85
Vynvan <68636193+Vynvan@users.noreply.github.com>
2024-11-05 01:21:41 +0100

    Little refactor

--- src/components/CardLayout.js
+++ src/components/CardLayout.js
@@ -4,7 +4,6 @@ import { Card, Container } from 'react-bootstrap';
 function CardLayout({ children, asArticle }) {
    if (children && !children.length) children = [children];
    const [visibleChildren, setVisibleChildren] = useState({});
-   const setVisible = index => value => setVisibleChildren({ ...visibleChildren, [index]: value });
 
    useEffect(() => {
       if (children && children.length > 0) {
@@ -17,11 +16,14 @@ function CardLayout({ children, asArticle }) {
    return (
       <Container as="main" className='flex-grow-1'>
          {children.map((child, index) => (
-            visibleChildren[index] !== false && <Card
+            visibleChildren[index] !== false && 
+            <Card
                as={asArticle ? 'article' : 'section'}
                className="mb-5 justify-content-center text-start shadow"
                key={index}>
-               {cloneElement(child, { setVisible: setVisible(index) })}
+               {cloneElement(child, {
+                  setVisible: value => setVisibleChildren({ ...visibleChildren, [index]: value })
+               })}
             </Card>
          ))}
       </Container>
